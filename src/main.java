bdbca40ba93a6b1652ea40fcdcf4b34c17f6d1e8
Saul Corona <saulcoronascs@gmail.com>
2021-07-31 00:06:54 -0500

    first commit

---
+++ src/main.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+
+/*Boton
+class myJButton  {
+    public static void main(String args[] ) {
+        JFrame f= new JFrame();
+        f.setTitle("myJButton");
+        f.setVisible(true);
+        f.setSize(1000,500);
+        Container c= f.getContentPane();
+        c.setLayout(null);
+        //creating object of JButton class and passing String to it's constructor
+        JButton btn=new JButton("click me ");
+        //Setting the location and size of the JButton
+        btn.setBounds(50,50,100,50);
+        //adding JButton to the frame's content pane
+        f.add(btn);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+*/
+
+/* //myJcheckbox
+
+class myJcheckbox extends JFrame{
+    // frame
+    static JFrame f;
+
+    public static void main(String args[] ) {
+
+
+        f = new JFrame("frame");
+        f.setLayout(new FlowLayout());
+
+        JCheckBox c1 = new JCheckBox("checkbox 1");
+        JCheckBox c2 = new JCheckBox("checkbox 2");
+
+        // create a new panel
+        JPanel p = new JPanel();
+
+        // add checkbox to panel
+        p.add(c1);
+        p.add(c2);
+
+        // add panel to frame
+        f.add(p);
+
+        // set the size of frame
+        f.setSize(300, 300);
+
+        f.show();
+    }
+}
+*/
